0e18614ca5442b05bb42476ddb5a36b11061aac6
JuliaChopyk <chopyk.julia@gmail.com>
2023-10-23 20:12:56 +0300

    hw part 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,78 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './section/section';
+import { FeedbackOptions } from './feedbackOptions/feedbackOptions';
+import { Statistics } from './statistics/statistics';
+import { Notification } from './notification/notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = e => {
+    const option = e.target.textContent;
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+
+  getKeys = obj => {
+    return Object.keys(obj);
+  };
+
+  getValues = obj => {
+    return Object.values(obj);
+  };
+
+  getEntries = obj => {
+    return Object.entries(obj);
+  };
+
+  countTotalFeedback = () => {
+    const values = this.getValues(this.state);
+    return values.reduce((total, value) => total + value, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    return parseInt((this.state.good / total) * 100);
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          minHeight: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          gap: '15px',
+          fontSize: 40,
+        }}
+      >
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.getKeys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          ></FeedbackOptions>
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback"></Notification>
+          ) : (
+            <Statistics
+              options={this.getEntries(this.state)}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            ></Statistics>
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
